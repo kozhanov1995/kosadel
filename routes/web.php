15819692cda95ef4627ecc3a26550e7a0c104af8
kozhanov1995 <kozhanov9525@gmail.com>
2024-01-14 01:54:38 +0600

    web.php update

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//создание/обновление ранга
 Route::get('/update-rank/{userId}/{rank}', [UserRankingController::class, 'updateRank']);
+
+//просмотр ранга
 Route::get('/get-ranks', [UserRankingController::class, 'getRanks']);
 
+//отправка уведомления пользователю по id
 Route::get('/send-notification/{userId}', [PushNotificationController::class, 'sendNotification']);
